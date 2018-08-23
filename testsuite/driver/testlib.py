DavidEichmann/ghc

testsuite: Save performance metrics in git notes.
Summary: Can now load up git note data into the python test driver.

Reviewers: bgamari

Subscribers: rwbarton, carter

Differential Revision: https://phabricator.haskell.org/D5059

GHC Trac Issues: #12758

testsuite/driver/testlib.py
@@ -19,7 +19,8 @@ import collections
 import subprocess
 
 from testglobals import config, ghc_env, default_testopts, brokens, t
-from testutil import strip_quotes, lndir, link_or_copy_file
+from testutil import strip_quotes, lndir, link_or_copy_file, passed, failBecause
+import perf_notes as Perf
 extra_src_files = {'T4198': ['exitminus1.c']} # TODO: See #12223
 
 global pool_sema
@@ -56,9 +57,13 @@ def setLocalTestOpts(opts):
     global testopts_local
     testopts_local.x=opts
 
+def isCompilerStatsTest():
+    opts = getTestOpts()
+    return bool(opts.is_compiler_stats_test)
+
 def isStatsTest():
     opts = getTestOpts()
-    return bool(opts.compiler_stats_range_fields or opts.stats_range_fields)
+    return bool(opts.stats_range_fields)
 
 
 # This can be called at the top of a file of tests, to set default test options
@@ -254,14 +259,14 @@ def _exit_code( name, opts, v ):
 
 def signal_exit_code( val ):
     if opsys('solaris2'):
-        return exit_code( val );
+        return exit_code( val )
     else:
         # When application running on Linux receives fatal error
         # signal, then its exit code is encoded as 128 + signal
         # value. See http://www.tldp.org/LDP/abs/html/exitcodes.html
         # I assume that Mac OS X behaves in the same way at least Mac
         # OS X builder behavior suggests this.
-        return exit_code( val+128 );
+        return exit_code( val+128 )
 
 # -----
 
@@ -307,42 +312,89 @@ def _extra_files(name, opts, files):
 
 # -----
 
-def stats_num_field( field, expecteds ):
-    return lambda name, opts, f=field, e=expecteds: _stats_num_field(name, opts, f, e);
+# Defaults to "test everything, and only break on extreme cases"
+#
+# The inputs to this function are slightly interesting:
+# metric can be either:
+#     - 'all', in which case all 3 possible metrics are collected and compared.
+#     - The specific metric one wants to use in the test.
+#     - A list of the metrics one wants to use in the test.
+#
+# Deviation defaults to 20% because the goal is correctness over performance.
+# The testsuite should avoid breaking when there is not an actual error.
+# Instead, the testsuite should notify of regressions in a non-breaking manner.
+#
+# collect_compiler_stats is used when the metrics collected are about the compiler.
+# collect_stats is used in the majority case when the metrics to be collected
+# are about the performance of the runtime code generated by the compiler.
+def collect_compiler_stats(metric='all',deviation=20):
+    return lambda name, opts, m=metric, d=deviation: _collect_stats(name, opts, m,d, True)
 
-def _stats_num_field( name, opts, field, expecteds ):
-    if field in opts.stats_range_fields:
-        framework_fail(name, 'duplicate-numfield', 'Duplicate ' + field + ' num_field check')
+def collect_stats(metric='all', deviation=20):
+    return lambda name, opts, m=metric, d=deviation: _collect_stats(name, opts, m, d)
 
-    if type(expecteds) is list:
-        for (b, expected, dev) in expecteds:
-            if b:
-                opts.stats_range_fields[field] = (expected, dev)
-                return
-        framework_warn(name, 'numfield-no-expected', 'No expected value found for ' + field + ' in num_field check')
+def testing_metrics():
+    return ['bytes allocated', 'peak_megabytes_allocated', 'max_bytes_used']
 
-    else:
-        (expected, dev) = expecteds
-        opts.stats_range_fields[field] = (expected, dev)
+# 'is_compiler_stats_test' is somewhat of an unfortunate name.
+# If the boolean is set to true, it indicates that this test is one that
+# measures the performance numbers of the compiler.
+# As this is a fairly rare case in the testsuite, it defaults to false to
+# indicate that it is a 'normal' performance test.
+# This is an internal function that is used only in the implementation.
+def _collect_stats(name, opts, metric, deviation, is_compiler_stats_test=False):
+    if not re.match('^[0-9]*[a-zA-Z][a-zA-Z0-9._-]*$', name):
+        # my_framework_fail(name, 'bad_name', 'This test has an invalid name')
+        my_failBecause('This test has an invalid name.')
 
-def compiler_stats_num_field( field, expecteds ):
-    return lambda name, opts, f=field, e=expecteds: _compiler_stats_num_field(name, opts, f, e);
+    tests = Perf.get_perf_stats('HEAD^')
 
-def _compiler_stats_num_field( name, opts, field, expecteds ):
-    if field in opts.compiler_stats_range_fields:
-        framework_fail(name, 'duplicate-numfield', 'Duplicate ' + field + ' num_field check')
+
+    # TODO do we need to filter for environmet and way etc.?
+
+
+    # Might have multiple metrics being measured for a single test.
+    test = [t for t in tests if t['test'] == name]
+
+    if tests == [] or test == []:
+        # There are no prior metrics for this test.
+        if isinstance(metric, str):
+            if metric == 'all':
+                for field in testing_metrics():
+                    opts.stats_range_fields[field] = None
+            else:
+                opts.stats_range_fields[metric] = None
+        if isinstance(metric, list):
+            for field in metric:
+                opts.stats_range_fields[field] = None
+
+        return
+
+    if is_compiler_stats_test:
+        opts.is_compiler_stats_test = True
 
     # Compiler performance numbers change when debugging is on, making the results
     # useless and confusing. Therefore, skip if debugging is on.
-    if compiler_debugged():
-        skip(name, opts)
+    if config.compiler_debugged and is_compiler_stats_test:
+        opts.skip = 1
 
-    for (b, expected, dev) in expecteds:
-        if b:
-            opts.compiler_stats_range_fields[field] = (expected, dev)
+    # 'all' is a shorthand to test for bytes allocated, peak megabytes allocated, and max bytes used.
+    if isinstance(metric, str):
+        if metric == 'all':
+            for field in testing_metrics():
+                # As there might be multiple "duplicates" of a test, the list
+                # comprehension considers the latest (ie the last item) to be
+                # the one we care about.
+                # (Ideally the list comprehension would result in a singleton list)
+                opts.stats_range_fields[field] = ([t['value'] for t in test if t['metric'] == field][-1], deviation)
+                return
+        else:
+            opts.stats_range_fields[metric] = ([t['value'] for t in test if t['metric'] == metric][-1], deviation)
             return
 
-    framework_warn(name, 'numfield-no-expected', 'No expected value found for ' + field + ' in num_field check')
+    if isinstance(metric, list):
+        for field in metric:
+            opts.stats_range_fields[field] = ([t['value'] for t in test if t['metric'] == field][-1], deviation)
 
 # -----
 
@@ -720,6 +772,7 @@ def test_common_work(watcher, name, opts, func, args):
             and (getTestOpts().only_ways == None or way in getTestOpts().only_ways) \
             and (config.cmdline_ways == [] or way in config.cmdline_ways) \
             and (not (config.skip_perf_tests and isStatsTest())) \
+            and (not (config.only_perf_tests and not isStatsTest())) \
             and way not in getTestOpts().omit_ways
 
         # Which ways we are asked to skip
@@ -927,12 +980,6 @@ def badResult(result):
     except (KeyError, TypeError):
         return True
 
-def passed():
-    return {'passFail': 'pass'}
-
-def failBecause(reason, tag=None):
-    return {'passFail': 'fail', 'reason': reason, 'tag': tag}
-
 # -----------------------------------------------------------------------------
 # Generic command tests
 
@@ -1087,56 +1134,64 @@ def multi_compile_and_run( name, way, top_mod, extra_mods, extra_hc_opts ):
 
 def stats( name, way, stats_file ):
     opts = getTestOpts()
-    return checkStats(name, way, stats_file, opts.stats_range_fields)
+    return check_stats(name, way, stats_file, opts.stats_range_fields)
+
+def metric_dict(name, way, metric, value):
+    return {
+        'test_env' : config.test_env,
+        'test'     : name,
+        'way'      : way,
+        'metric'   : metric,
+        'value'    : value
+    }
 
 # -----------------------------------------------------------------------------
-# Check -t stats info
-
-def checkStats(name, way, stats_file, range_fields):
-    full_name = name + '(' + way + ')'
-
+# Check test stats. This prints the results for the user.
+# name: name of the test.
+# way: the way.
+# stats_file: the path of the stats_file containing the stats for the test.
+# range_fields
+# Returns a pass/fail object. Passes if the stats are withing the expected value ranges.
+# This prints the results for the user.
+def check_stats(name, way, stats_file, range_fields):
     result = passed()
     if range_fields:
         try:
             f = open(in_testdir(stats_file))
         except IOError as e:
             return failBecause(str(e))
-        contents = f.read()
+        stats_file_contents = f.read()
         f.close()
 
-        for (field, (expected, dev)) in range_fields.items():
-            m = re.search('\("' + field + '", "([0-9]+)"\)', contents)
-            if m == None:
-                print('Failed to find field: ', field)
-                result = failBecause('no such stats field')
-            val = int(m.group(1))
+        for (metric, range_val_dev) in range_fields.items():
+            field_match = re.search('\("' + metric + '", "([0-9]+)"\)', stats_file_contents)
+            if field_match == None:
+                print('Failed to find metric: ', metric)
+                result = failBecause('no such stats metric')
+            else:
+                actual_val = int(field_match.group(1))
+                
+                # Store the metric so it can later be stored in a git note.
+                t.metrics.append(metric_dict(name, way, metric, actual_val))
 
-            lowerBound = trunc(           expected * ((100 - float(dev))/100))
-            upperBound = trunc(0.5 + ceil(expected * ((100 + float(dev))/100)))
+                # If this is the first time running the benchmark, then fail.
+                if range_val_dev == None:
+                    return failBecause('no expected stat found (this may be the first recording this stat)')
 
-            deviation = round(((float(val) * 100)/ expected) - 100, 1)
+                (expected_val, tolerance_dev) = range_val_dev
+                result = Perf.check_stats(
+                    name,
+                    metric,
+                    way,
+                    expected_val,
+                    actual_val,
+                    tolerance_dev,
+                    config.verbose >= 4)
 
-            if val < lowerBound:
-                print(field, 'value is too low:')
-                print('(If this is because you have improved GHC, please')
-                print('update the test so that GHC doesn\'t regress again)')
-                result = failBecause('stat too good', tag='stat')
-            if val > upperBound:
-                print(field, 'value is too high:')
-                result = failBecause('stat not good enough', tag='stat')
 
-            if val < lowerBound or val > upperBound or config.verbose >= 4:
-                length = max(len(str(x)) for x in [expected, lowerBound, upperBound, val])
-
-                def display(descr, val, extra):
-                    print(descr, str(val).rjust(length), extra)
-
-                display('    Expected    ' + full_name + ' ' + field + ':', expected, '+/-' + str(dev) + '%')
-                display('    Lower bound ' + full_name + ' ' + field + ':', lowerBound, '')
-                display('    Upper bound ' + full_name + ' ' + field + ':', upperBound, '')
-                display('    Actual      ' + full_name + ' ' + field + ':', val, '')
-                if val != expected:
-                    display('    Deviation   ' + full_name + ' ' + field + ':', deviation, '%')
+            # If the test fails a metric, exit immediately.
+            if result['passFail'] == 'fail':
+                return result
 
     return result
 
@@ -1186,7 +1241,7 @@ def simple_build(name, way, extra_hc_opts, should_fail, top_mod, link, addsuf, b
         to_do = '-c' # just compile
 
     stats_file = name + '.comp.stats'
-    if opts.compiler_stats_range_fields:
+    if isCompilerStatsTest():
         extra_hc_opts += ' +RTS -V0 -t' + stats_file + ' --machine-readable -RTS'
     if backpack:
         extra_hc_opts += ' -outputdir ' + name + '.out'
@@ -1219,10 +1274,10 @@ def simple_build(name, way, extra_hc_opts, should_fail, top_mod, link, addsuf, b
 
     # ToDo: if the sub-shell was killed by ^C, then exit
 
-    statsResult = checkStats(name, way, stats_file, opts.compiler_stats_range_fields)
-
-    if badResult(statsResult):
-        return statsResult
+    if isCompilerStatsTest():
+        statsResult = check_stats(name, way, stats_file, opts.stats_range_fields)
+        if badResult(statsResult):
+            return statsResult
 
     if should_fail:
         if exit_code == 0:
@@ -1260,7 +1315,7 @@ def simple_run(name, way, prog, extra_run_opts):
     my_rts_flags = rts_flags(way)
 
     stats_file = name + '.stats'
-    if opts.stats_range_fields:
+    if isStatsTest() and not isCompilerStatsTest():
         stats_args = ' +RTS -V0 -t' + stats_file + ' --machine-readable -RTS'
     else:
         stats_args = ''
@@ -1298,7 +1353,7 @@ def simple_run(name, way, prog, extra_run_opts):
     if check_prof and not check_prof_ok(name, way):
         return failBecause('bad profile')
 
-    return checkStats(name, way, stats_file, opts.stats_range_fields)
+    return check_stats(name, way, stats_file, opts.stats_range_fields)
 
 def rts_flags(way):
     args = config.way_rts_flags.get(way, [])

watashi/fbghc

testsuite/driver/testlib.py
@@ -19,7 +19,8 @@ import collections
 import subprocess
 
 from testglobals import config, ghc_env, default_testopts, brokens, t
-from testutil import strip_quotes, lndir, link_or_copy_file
+from testutil import strip_quotes, lndir, link_or_copy_file, passed, failBecause
+import perf_notes as Perf
 extra_src_files = {'T4198': ['exitminus1.c']} # TODO: See #12223
 
 global pool_sema
@@ -56,9 +57,13 @@ def setLocalTestOpts(opts):
     global testopts_local
     testopts_local.x=opts
 
+def isCompilerStatsTest():
+    opts = getTestOpts()
+    return bool(opts.is_compiler_stats_test)
+
 def isStatsTest():
     opts = getTestOpts()
-    return bool(opts.compiler_stats_range_fields or opts.stats_range_fields)
+    return bool(opts.stats_range_fields)
 
 
 # This can be called at the top of a file of tests, to set default test options
@@ -254,14 +259,14 @@ def _exit_code( name, opts, v ):
 
 def signal_exit_code( val ):
     if opsys('solaris2'):
-        return exit_code( val );
+        return exit_code( val )
     else:
         # When application running on Linux receives fatal error
         # signal, then its exit code is encoded as 128 + signal
         # value. See http://www.tldp.org/LDP/abs/html/exitcodes.html
         # I assume that Mac OS X behaves in the same way at least Mac
         # OS X builder behavior suggests this.
-        return exit_code( val+128 );
+        return exit_code( val+128 )
 
 # -----
 
@@ -307,42 +312,87 @@ def _extra_files(name, opts, files):
 
 # -----
 
-def stats_num_field( field, expecteds ):
-    return lambda name, opts, f=field, e=expecteds: _stats_num_field(name, opts, f, e);
+# Defaults to "test everything, and only break on extreme cases"
+#
+# The inputs to this function are slightly interesting:
+# metric can be either:
+#     - 'all', in which case all 3 possible metrics are collected and compared.
+#     - The specific metric one wants to use in the test.
+#     - A list of the metrics one wants to use in the test.
+#
+# Deviation defaults to 20% because the goal is correctness over performance.
+# The testsuite should avoid breaking when there is not an actual error.
+# Instead, the testsuite should notify of regressions in a non-breaking manner.
+#
+# collect_compiler_stats is used when the metrics collected are about the compiler.
+# collect_stats is used in the majority case when the metrics to be collected
+# are about the performance of the runtime code generated by the compiler.
+def collect_compiler_stats(metric='all',deviation=20):
+    return lambda name, opts, m=metric, d=deviation: _collect_stats(name, opts, m,d, True)
 
-def _stats_num_field( name, opts, field, expecteds ):
-    if field in opts.stats_range_fields:
-        framework_fail(name, 'duplicate-numfield', 'Duplicate ' + field + ' num_field check')
+def collect_stats(metric='all', deviation=20):
+    return lambda name, opts, m=metric, d=deviation: _collect_stats(name, opts, m, d)
 
-    if type(expecteds) is list:
-        for (b, expected, dev) in expecteds:
-            if b:
-                opts.stats_range_fields[field] = (expected, dev)
-                return
-        framework_warn(name, 'numfield-no-expected', 'No expected value found for ' + field + ' in num_field check')
+def testing_metrics():
+    return ['bytes allocated', 'peak_megabytes_allocated', 'max_bytes_used']
 
-    else:
-        (expected, dev) = expecteds
-        opts.stats_range_fields[field] = (expected, dev)
+# 'is_compiler_stats_test' is somewhat of an unfortunate name.
+# If the boolean is set to true, it indicates that this test is one that
+# measures the performance numbers of the compiler.
+# As this is a fairly rare case in the testsuite, it defaults to false to
+# indicate that it is a 'normal' performance test.
+# This is an internal function that is used only in the implementation.
+def _collect_stats(name, opts, metric, deviation, is_compiler_stats_test=False):
+    if not re.match('^[0-9]*[a-zA-Z][a-zA-Z0-9._-]*$', name):
+        failBecause('This test has an invalid name.')
 
-def compiler_stats_num_field( field, expecteds ):
-    return lambda name, opts, f=field, e=expecteds: _compiler_stats_num_field(name, opts, f, e);
+    tests = Perf.get_perf_stats('HEAD^')
 
-def _compiler_stats_num_field( name, opts, field, expecteds ):
-    if field in opts.compiler_stats_range_fields:
-        framework_fail(name, 'duplicate-numfield', 'Duplicate ' + field + ' num_field check')
+    # TODO do we need to filter for environmet and way etc.?
+
+
+    # Might have multiple metrics being measured for a single test.
+    test = [t for t in tests if t['test'] == name]
+
+    if tests == [] or test == []:
+        # There are no prior metrics for this test.
+        if isinstance(metric, str):
+            if metric == 'all':
+                for field in testing_metrics():
+                    opts.stats_range_fields[field] = None
+            else:
+                opts.stats_range_fields[metric] = None
+        if isinstance(metric, list):
+            for field in metric:
+                opts.stats_range_fields[field] = None
+
+        return
+
+    if is_compiler_stats_test:
+        opts.is_compiler_stats_test = True
 
     # Compiler performance numbers change when debugging is on, making the results
     # useless and confusing. Therefore, skip if debugging is on.
-    if compiler_debugged():
-        skip(name, opts)
+    if config.compiler_debugged and is_compiler_stats_test:
+        opts.skip = 1
 
-    for (b, expected, dev) in expecteds:
-        if b:
-            opts.compiler_stats_range_fields[field] = (expected, dev)
+    # 'all' is a shorthand to test for bytes allocated, peak megabytes allocated, and max bytes used.
+    if isinstance(metric, str):
+        if metric == 'all':
+            for field in testing_metrics():
+                # As there might be multiple "duplicates" of a test, the list
+                # comprehension considers the latest (ie the last item) to be
+                # the one we care about.
+                # (Ideally the list comprehension would result in a singleton list)
+                opts.stats_range_fields[field] = ([t['value'] for t in test if t['metric'] == field][-1], deviation)
+                return
+        else:
+            opts.stats_range_fields[metric] = ([t['value'] for t in test if t['metric'] == metric][-1], deviation)
             return
 
-    framework_warn(name, 'numfield-no-expected', 'No expected value found for ' + field + ' in num_field check')
+    if isinstance(metric, list):
+        for field in metric:
+            opts.stats_range_fields[field] = ([t['value'] for t in test if t['metric'] == field][-1], deviation)
 
 # -----
 
@@ -720,6 +770,7 @@ def test_common_work(watcher, name, opts, func, args):
             and (getTestOpts().only_ways == None or way in getTestOpts().only_ways) \
             and (config.cmdline_ways == [] or way in config.cmdline_ways) \
             and (not (config.skip_perf_tests and isStatsTest())) \
+            and (not (config.only_perf_tests and not isStatsTest())) \
             and way not in getTestOpts().omit_ways
 
         # Which ways we are asked to skip
@@ -927,12 +978,6 @@ def badResult(result):
     except (KeyError, TypeError):
         return True
 
-def passed():
-    return {'passFail': 'pass'}
-
-def failBecause(reason, tag=None):
-    return {'passFail': 'fail', 'reason': reason, 'tag': tag}
-
 # -----------------------------------------------------------------------------
 # Generic command tests
 
@@ -1087,56 +1132,66 @@ def multi_compile_and_run( name, way, top_mod, extra_mods, extra_hc_opts ):
 
 def stats( name, way, stats_file ):
     opts = getTestOpts()
-    return checkStats(name, way, stats_file, opts.stats_range_fields)
+    return check_stats(name, way, stats_file, opts.stats_range_fields)
+
+def metric_dict(name, way, metric, value):
+    return {
+        'test_env' : config.test_env,
+        'test'     : name,
+        'way'      : way,
+        'metric'   : metric,
+        'value'    : value
+    }
 
 # -----------------------------------------------------------------------------
-# Check -t stats info
-
-def checkStats(name, way, stats_file, range_fields):
-    full_name = name + '(' + way + ')'
-
+# Check test stats. This prints the results for the user.
+# name: name of the test.
+# way: the way.
+# stats_file: the path of the stats_file containing the stats for the test.
+# range_fields
+# Returns a pass/fail object. Passes if the stats are withing the expected value ranges.
+# This prints the results for the user.
+def check_stats(name, way, stats_file, range_fields):
     result = passed()
     if range_fields:
         try:
             f = open(in_testdir(stats_file))
         except IOError as e:
             return failBecause(str(e))
-        contents = f.read()
+        stats_file_contents = f.read()
         f.close()
 
-        for (field, (expected, dev)) in range_fields.items():
-            m = re.search('\("' + field + '", "([0-9]+)"\)', contents)
-            if m == None:
-                print('Failed to find field: ', field)
-                result = failBecause('no such stats field')
-            val = int(m.group(1))
+        for (metric, range_val_dev) in range_fields.items():
+            field_match = re.search('\("' + metric + '", "([0-9]+)"\)', stats_file_contents)
+            if field_match == None:
+                print('Failed to find metric: ', metric)
+                result = failBecause('no such stats metric')
+            else:
+                actual_val = int(field_match.group(1))
+                
+                # Store the metric so it can later be stored in a git note.
+                t.metrics.append(metric_dict(name, way, metric, actual_val))
 
-            lowerBound = trunc(           expected * ((100 - float(dev))/100))
-            upperBound = trunc(0.5 + ceil(expected * ((100 + float(dev))/100)))
+                # If this is the first time running the benchmark, then pass.
+                if range_val_dev == None:
+                    result = passed()
+                else:
+                    (expected_val, tolerance_dev) = range_val_dev
+                    result = Perf.check_stats(
+                        name,
+                        metric,
+                        config.test_env,
+                        way,
+                        expected_val,
+                        actual_val,
+                        tolerance_dev,
+                        config.allowed_perf_changes,
+                        config.verbose >= 4)
 
-            deviation = round(((float(val) * 100)/ expected) - 100, 1)
 
-            if val < lowerBound:
-                print(field, 'value is too low:')
-                print('(If this is because you have improved GHC, please')
-                print('update the test so that GHC doesn\'t regress again)')
-                result = failBecause('stat too good', tag='stat')
-            if val > upperBound:
-                print(field, 'value is too high:')
-                result = failBecause('stat not good enough', tag='stat')
-
-            if val < lowerBound or val > upperBound or config.verbose >= 4:
-                length = max(len(str(x)) for x in [expected, lowerBound, upperBound, val])
-
-                def display(descr, val, extra):
-                    print(descr, str(val).rjust(length), extra)
-
-                display('    Expected    ' + full_name + ' ' + field + ':', expected, '+/-' + str(dev) + '%')
-                display('    Lower bound ' + full_name + ' ' + field + ':', lowerBound, '')
-                display('    Upper bound ' + full_name + ' ' + field + ':', upperBound, '')
-                display('    Actual      ' + full_name + ' ' + field + ':', val, '')
-                if val != expected:
-                    display('    Deviation   ' + full_name + ' ' + field + ':', deviation, '%')
+            # If the test fails a metric, exit immediately.
+            if result['passFail'] == 'fail':
+                return result
 
     return result
 
@@ -1186,7 +1241,7 @@ def simple_build(name, way, extra_hc_opts, should_fail, top_mod, link, addsuf, b
         to_do = '-c' # just compile
 
     stats_file = name + '.comp.stats'
-    if opts.compiler_stats_range_fields:
+    if isCompilerStatsTest():
         extra_hc_opts += ' +RTS -V0 -t' + stats_file + ' --machine-readable -RTS'
     if backpack:
         extra_hc_opts += ' -outputdir ' + name + '.out'
@@ -1219,10 +1274,10 @@ def simple_build(name, way, extra_hc_opts, should_fail, top_mod, link, addsuf, b
 
     # ToDo: if the sub-shell was killed by ^C, then exit
 
-    statsResult = checkStats(name, way, stats_file, opts.compiler_stats_range_fields)
-
-    if badResult(statsResult):
-        return statsResult
+    if isCompilerStatsTest():
+        statsResult = check_stats(name, way, stats_file, opts.stats_range_fields)
+        if badResult(statsResult):
+            return statsResult
 
     if should_fail:
         if exit_code == 0:
@@ -1260,7 +1315,7 @@ def simple_run(name, way, prog, extra_run_opts):
     my_rts_flags = rts_flags(way)
 
     stats_file = name + '.stats'
-    if opts.stats_range_fields:
+    if isStatsTest() and not isCompilerStatsTest():
         stats_args = ' +RTS -V0 -t' + stats_file + ' --machine-readable -RTS'
     else:
         stats_args = ''
@@ -1298,7 +1353,7 @@ def simple_run(name, way, prog, extra_run_opts):
     if check_prof and not check_prof_ok(name, way):
         return failBecause('bad profile')
 
-    return checkStats(name, way, stats_file, opts.stats_range_fields)
+    return check_stats(name, way, stats_file, opts.stats_range_fields)
 
 def rts_flags(way):
     args = config.way_rts_flags.get(way, [])
@@ -1993,7 +2048,7 @@ def findTFiles(roots):
 # -----------------------------------------------------------------------------
 # Output a test summary to the specified file object
 
-def summary(t, file, short=False):
+def summary(t, file, short=False, color=False):
 
     file.write('\n')
     printUnexpectedTests(file,
@@ -2004,7 +2059,18 @@ def summary(t, file, short=False):
         # Only print the list of unexpected tests above.
         return
 
-    file.write('SUMMARY for test run started at '
+    color_start = ''
+    color_end = ''
+    if color:
+        color_end = '\033[0m'
+        if len(t.unexpected_failures) > 0 or \
+            len(t.unexpected_stat_failures) > 0 or \
+            len(t.framework_failures) > 0:
+            color_start = '\033[1m\033[43m\033[31m'
+        else:
+            color_start = '\033[1m\033[32m'
+
+    file.write(color_start + 'SUMMARY' + color_end + ' for test run started at '
                + time.strftime("%c %Z", t.start_time) + '\n'
                + str(datetime.timedelta(seconds=
                     round(time.time() - time.mktime(t.start_time)))).rjust(8)